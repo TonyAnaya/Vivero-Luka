3684390ad9c76557632d8fd3a3775c42e566a416
TonyAnaya <jaanaya99@gmail.com>
2022-04-01 13:59:23 -0600

    [FIXED] Componente Cifras

--- Vivero-Luka/src/components/Catalogo/Catalogo.jsx
+++ Vivero-Luka/src/components/Catalogo/Catalogo.jsx
@@ -37,18 +37,112 @@ const Catalogo = () => {
                             </div>
                         </div>
                     </div>
-
-
-
-
-
-
-
-
-
                 </div>
             </div>
         </div>
+        
+        <div className="col-3"> 
+            <div className="card" >
+                <img src={Cancha} className="card-img-top imag" alt="..."/>
+                <div className="card-body">
+                    <h5 className="card-title">Planta numero 2</h5>
+                    <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal2">
+                        Ver Más
+                    </button>
+
+                    <div className="modal fade" id="exampleModal2" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div className="modal-dialog">
+                            <div className="modal-content">
+                                <div className="modal-header">
+                                    <h5 className="modal-title" id="exampleModalLabel">Planta numero 2</h5>
+                                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div className="modal-body">
+                                    
+                                    <div className="card" >
+                                        <img src={Cancha} className="card-img-top imag" alt="..."/>
+                                        <p>Aqui va la descripcion de la planta numero 2, chance tambien se puede poner un poquito de la historia o las características de la misma</p>
+                                    </div>
+                                </div>
+                                <div className="modal-footer">
+                                    <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div className="col-3"> 
+            <div className="card" >
+                <img src={Cancha} className="card-img-top imag" alt="..."/>
+                <div className="card-body">
+                    <h5 className="card-title">Planta numero 3</h5>
+                    <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal3">
+                        Ver Más
+                    </button>
+
+                    <div className="modal fade" id="exampleModal3" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div className="modal-dialog">
+                            <div className="modal-content">
+                                <div className="modal-header">
+                                    <h5 className="modal-title" id="exampleModalLabel">Planta numero 3</h5>
+                                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div className="modal-body">
+                                    
+                                    <div className="card" >
+                                        <img src={Cancha} className="card-img-top imag" alt="..."/>
+                                        <p>Aqui va la descripcion de la planta numero 3, chance tambien se puede poner un poquito de la historia o las características de la misma</p>
+                                    </div>
+                                </div>
+                                <div className="modal-footer">
+                                    <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <div className="col-3"> 
+            <div className="card" >
+                <img src={Cancha} className="card-img-top imag" alt="..."/>
+                <div className="card-body">
+                    <h5 className="card-title">Planta numero 4</h5>
+                    <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal4">
+                        Ver Más
+                    </button>
+
+                    <div className="modal fade" id="exampleModal4" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div className="modal-dialog">
+                            <div className="modal-content">
+                                <div className="modal-header">
+                                    <h5 className="modal-title" id="exampleModalLabel">Planta numero 4</h5>
+                                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div className="modal-body">
+                                    
+                                    <div className="card" >
+                                        <img src={Cancha} className="card-img-top imag" alt="..."/>
+                                        <p>Aqui va la descripcion de la planta numero 4, chance tambien se puede poner un poquito de la historia o las características de la misma</p>
+                                    </div>
+                                </div>
+                                <div className="modal-footer">
+                                    <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
     </div>
   )
 }
