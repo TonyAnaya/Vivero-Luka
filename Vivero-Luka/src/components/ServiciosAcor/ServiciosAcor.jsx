f12d539cda29bebedf7fe7cb48aede78ed711f0e
Mariano Estrada <mariano-e-s@hotmail.com>
2022-05-18 16:01:51 -0500

    galeria

--- Vivero-Luka/src/components/ServiciosAcor/ServiciosAcor.jsx
+++ Vivero-Luka/src/components/ServiciosAcor/ServiciosAcor.jsx
@@ -1,6 +1,15 @@
 import "./ServiciosAcor.css";
 import arboles from "./assets/vivero.jpg";
 import prueba from "./assets/prueba.jpg"
+import foto1 from './assets/foto1.jpg'
+import foto2 from './assets/foto2.jpg'
+import foto3 from './assets/foto3.jpg'
+import foto4 from './assets/foto4.jpg'
+import foto5 from './assets/foto5.jpg'
+import foto6 from './assets/foto6.jpg'
+import foto7 from './assets/foto7.jpg'
+import foto8 from './assets/foto8.jpg'
+import foto9 from './assets/foto9.jpg'
 
 
 const ServiciosAcor = () => {
@@ -13,18 +22,18 @@ const ServiciosAcor = () => {
         <img src={arboles} alt="" />
       </div>
       <br />
-      <h1 className="tituloServicios">Algunos de nuestros trabajos</h1>
+      <h1 className="tituloServicios">Galería</h1>
       <br />
       <div className="serviciosFlex">
-        <img src={prueba} alt="img" className="imgFlexServ"/>
-        <img src={prueba} alt="img" className="imgFlexServ"/>
-        <img src={prueba} alt="img" className="imgFlexServ"/>
-        <img src={prueba} alt="img" className="imgFlexServ"/>
-        <img src={prueba} alt="img" className="imgFlexServ"/>
-        <img src={prueba} alt="img" className="imgFlexServ"/>
-        <img src={prueba} alt="img" className="imgFlexServ"/>
-        <img src={prueba} alt="img" className="imgFlexServ"/>
-        <img src={prueba} alt="img" className="imgFlexServ"/>
+        <img src={foto1} alt="img" className="imgFlexServ"/>
+        <img src={foto2} alt="img" className="imgFlexServ"/>
+        <img src={foto3} alt="img" className="imgFlexServ"/>
+        <img src={foto4} alt="img" className="imgFlexServ"/>
+        <img src={foto5} alt="img" className="imgFlexServ"/>
+        <img src={foto6} alt="img" className="imgFlexServ"/>
+        <img src={foto7} alt="img" className="imgFlexServ"/>
+        <img src={foto8} alt="img" className="imgFlexServ"/>
+        <img src={foto9} alt="img" className="imgFlexServ"/>
       </div>
       <br />
       <h1 className="tituloServicios">Consejos y cuidados</h1>
